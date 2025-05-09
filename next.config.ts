cfd9f73cc1948dac388e18d5b39e3f62925d7ff4
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-05-09 17:22:56 -0400

    Trying in the GHA for PR etc.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = (phase: any) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+
+  const nextConfig: NextConfig = {
+    output: isDev ? undefined : "export",
+    distDir: isDev ? undefined : "dist",
+  };
+  return nextConfig;
 };
 
 export default nextConfig;
